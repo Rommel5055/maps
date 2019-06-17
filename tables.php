96c7cc1076a872748c9b3467f1469b9857647c6f
Javier Gonzalez <javiergonzalez@alumnos.uai.cl>
2019-06-17 03:48:31 -0400

    Add files via upload

---
+++ tables.php
@@ -0,0 +1,87 @@
+/*
+<?php
+$serverName = "grupo5arqui.database.windows.net"; //serverName\instanceName, portNumber (default is 1433)
+$connectionInfo = array( "Database"=>"arqui", "UID"=>"grupo5", "PWD"=>"Arqui2019");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( $conn ) {
+     echo "Connection established.<br />";
+}else{
+     echo "Connection could not be established.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+?>
+*/
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Table V01</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/perfect-scrollbar/perfect-scrollbar.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>	
+	<div class="limiter">
+		<div class="container-table100">
+			<div class="wrap-table100">
+				<div class="table100">
+					<table>
+						<thead>
+							<tr class="table100-head">
+								<th class="column1">Nombre</th>
+								<th class="column2">Precio</th>
+								<th class="column3">Descripcion</th>
+								<th class="column4">Comprar</th>
+							</tr>
+						</thead>
+						<tbody>
+								<tr>
+									<td class="column1">Led 55 Pulgadas 4K</td>
+									<td class="column2">$269.990</td>
+									<td class="column3">Televisor</td>
+									<td class="column4">
+<a href="index.php" class="btn btn-primary">Back</a>
+										
+										<form action="registration.php" method="post">
+Comprar <input type="int" obj="obj">
+<input type="submit"></td>
+								</tr>
+								
+								
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	
+
+<!--===============================================================================================-->	
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
